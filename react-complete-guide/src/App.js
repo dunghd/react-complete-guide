99a5ca91724627764c2213aa86ff71516576ce57
Dung Ho <ddh@elca.vn>
2020-12-01 16:35:21 +0700

    Section 3 - Hook

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,21 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 },
+    ],
+  });
+
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(otherState);
+
+  console.log(personsState);
+
+  const switchNameHandler = () => {
+    // DON'T DO THIS: personsState.persons[0].name = 'DDH';
+    setPersonsState({
+      persons: [
+        { name: 'DDH', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 },
+      ],
+      otherState: personsState.otherState
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>TEST TEST TEST</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+    </div>
+  );
+  // return React.createElement('div', {className: 'App' }, React.createElement('h1', null, `Does it work now?`));
 }
 
-export default App;
+export default app;
